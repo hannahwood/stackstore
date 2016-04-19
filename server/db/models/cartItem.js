9c4f45ae744f324cbc787b209d853f7b3131db55
David Zausner <zausnerd@gmail.com>
2016-04-19 10:43:11 -0400

    caritem modified

---
+++ server/db/models/cartItem.js
@@ -0,0 +1,30 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+	finalPrice: {
+		type: Number,
+		required: true
+	},
+	quantity: {
+		type: Number,
+		required: true,
+		validate: {
+			validator: function(amount) {
+				return Math.floor(amount) === amount;
+			},
+			message: 'Amount must be a whole number'
+		}
+	},
+	product: {
+		type: mongoose.Schema.ObjectId, ref: 'Product',
+		required: true
+	},
+	order: {
+		type:Schema.ObjectId, ref: 'Order',
+		required: true
+	}
+});
+
+
+mongoose.model('CartItem', cartSchema);
