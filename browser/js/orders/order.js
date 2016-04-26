67e3eba8c6e8e6f3e7bb551b1236e595330729b4
seanbert4 <seanbert4@gmail.com>
2016-04-26 11:47:18 -0400

    user profile and order page working

--- browser/js/orders/order.js
+++ browser/js/orders/order.js
@@ -13,7 +13,7 @@ app.config(function($stateProvider) {
     });
 });
 
-app.factory('CartOrderFactory', function($http) {
+app.factory('CartOrderFactory', function($http, ProductsFactory) {
     return {
         fetchOneOrder: function(orderId) {
             return $http.get('/api/orders/' + orderId)
@@ -34,16 +34,33 @@ app.factory('CartOrderFactory', function($http) {
         },
         priceTotal: function(cartItems) {
             return cartItems.reduce(function(total, nextItem) {
-                return total += (nextItem.product.price * nextItem.quantity);
+                return total += (nextItem.price * nextItem.quantity);
             }, 0);
         },
+        formatDate: order => order.date.split("T")[0],
+        populateCartItems: function(order) {
+            return Promise.all(order.cartItems.map(function(cartItem) {
+                return $http.get('/api/products/' + cartItem.product)
+                .then(response => response.data)
+                .then(productObj => {
+                    cartItem.product = productObj.product;
+                    return cartItem;
+                });
+            }));
+        }
     };
 });
 
-app.controller('OrderCtrl', function($scope, CartOrderFactory, order) {
+app.controller('OrderCtrl', function($log, $scope, CartOrderFactory, order) {
     $scope.quantityTotal = CartOrderFactory.quantityTotal(order.cartItems);
     $scope.item = CartOrderFactory.item($scope.quantityTotal);
     $scope.priceTotal = CartOrderFactory.priceTotal(order.cartItems);
-    $scope.date = order.date;
-    $scope.items = order.cartItems;
+    $scope.date = CartOrderFactory.formatDate(order);
+
+    CartOrderFactory.populateCartItems(order)
+    .then(cartItems => {
+        $scope.items = cartItems;
+        $scope.$evalAsync();
+    })
+    .catch($log.error);
 });
